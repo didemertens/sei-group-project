cc68c2c8e3f5133652c6a2bea61366d3ba79717c
didemertens <d.g.j.mertens@gmail.com>
2020-02-07 16:59:17 +0000

    fixed mongoparse error

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const env = process.env.NODE_ENV || 'development'
 const dbURI = env === 'production' ? process.env.MONGODB_URI : `${process.env.MONGODB_URI}-${env}`
 
 const port = process.env.PORT || 4000
 // const secret = process.env.SECRET || 'shhh its a secret'
 
-module.exports = { env, port,
+module.exports = {
+  env, port,
   dbURI
   // secret
 }
