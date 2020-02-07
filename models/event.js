1f8912c5170b42f7468729697d4680b46fb956df
georgeanthony33 <georgeanthony33@gmail.com>
2020-02-07 14:39:06 +0000

    created event schema

---
+++ models/event.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: Date, required: true },
+  location: { type: String, required: true },
+  postcode: { type: String, required: true },
+  description: { type: String, required: true, maxlength: 1000 },
+  requiredPeople: { type: Number, required: true }
+  // user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
