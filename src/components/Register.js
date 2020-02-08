19550cf924dfab2a6201e8a7476a6a0234f930d2
ckapak <christinekapak@googlemail.com>
2020-02-08 13:01:59 +0000

    added component folder and added register front page

---
+++ src/components/Register.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import axios from 'axios'
+
+class Register extends React.Component {
+  state = {
+    data: {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    },
+    errors: {}
+  }
+
+  handleChange = e => {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    const errors = { ...this.state.errors, [e.target.name]: '' }
+    this.setState({ data, errors })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+    
+    try {
+      await axios.post('/api/register', this.state.data)
+    } catch (err) {
+      this.setState({ errors: err.response.data.errors })
+    }
+  }
+
+
+  render() {
+    return (
+      <h1>Register</h1>
+    )
+  }
+}
+
+export default Register
