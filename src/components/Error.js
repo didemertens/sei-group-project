18724d144c14683ba17c9f6fc5c288ff751a3075
Jesse Golding <jessegolding05@gmail.com>
2020-02-09 17:31:13 +0000

    error page updated

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ErrorPage = () => (
+  <section className="section hero">
+    <div className="container">
+      <h1 className="u-full-width">Event is no longer listed.</h1>
+    </div>
+
+  </section>
+)
+
+export default ErrorPage
