b7053e425f0e038ad3bc15f6798041385d88e404
didemertens <d.g.j.mertens@gmail.com>
2020-02-09 15:19:01 +0000

    added filters for events

--- src/components/events/EventIndex.js
+++ src/components/events/EventIndex.js
@@ -5,7 +5,8 @@ import moment from 'moment'
 
 class EventIndex extends React.Component {
   state = {
-    events: []
+    events: [],
+    otherSuggestion: ''
   }
 
   async componentDidMount() {
@@ -13,21 +14,74 @@ class EventIndex extends React.Component {
     const searchData = this.props.history.location.state.searchData
     try {
       const res = await axios.get('/api/events')
-      // for now, just filter on category
-      const events = res.data.filter(event => {
-        return event.category === searchData.category
-      })
-      this.setState({ events })
-
-      // filter on date
-      // const events = res.data.filter(event => {
-      //   return moment(event.date).isSame(searchData.date, 'day') // will check year and month and day
-      // })
+      this.filterEvents(res, searchData)
     } catch (err) {
       console.log(err)
       this.props.history.push('/error')
     }
   }
+
+  // filter on: 1) category, 2) location, 3) date, 4) time
+  filterEvents = (res, searchData) => {
+    console.log(searchData)
+    let events = []
+    const date = moment(searchData.date).format('DD/MM/YYYY')
+
+    // checking category
+    const categories = res.data.filter(event => {
+      return event.category === searchData.category
+    })
+
+    if (categories.length === 0) {
+      this.setState({
+        otherSuggestion: `There are no ${searchData.category} events yet. Go back and try another search.`
+      })
+      return
+    }
+
+    // checking postcode
+    const postcodes = res.data.filter(event => {
+      return event.postcode === searchData.postcode
+    })
+
+    if (postcodes.length === 0) {
+      this.setState({
+        otherSuggestion: 'There are no events in your area yet. Go back and try another search.'
+      })
+      return
+    }
+
+    // checking date
+    const dates = categories.filter(event => {
+      return moment(event.date).isSame(searchData.date, 'day') // checks year, month and day
+    })
+
+    if (dates.length === 0) {
+      this.setState({
+        otherSuggestion: `There are no events for ${date} yet. Here are some other suggestions.`
+      })
+      events = [...postcodes]
+      this.setState({ events })
+      return
+    }
+
+    // checking time
+    const times = dates.filter(event => {
+      return event.time === searchData.time
+    })
+
+    if (times.length === 0) {
+      this.setState({
+        otherSuggestion: `There are no events at ${searchData.time} yet. Here are some other suggestions for ${date}.`
+      })
+      events = [...dates]
+    } else {
+      events = [...times]
+    }
+
+    this.setState({ events })
+  }
+
 
   // handleChange = e => {
   //   e.preventDefault()
@@ -38,7 +92,7 @@ class EventIndex extends React.Component {
   // }
 
   render() {
-    const { events } = this.state
+    const { events, otherSuggestion } = this.state
     return (
       <section className="section">
         <div className="container">
@@ -47,6 +101,7 @@ class EventIndex extends React.Component {
             <div className="six columns">
               <p>Events</p>
               <div className="cards">
+                {otherSuggestion && <p>{otherSuggestion}</p>}
                 {events.map(event => (
                   <div className="card" key={event._id}>
                     <h5>{event.name}</h5>
