a005b1048fee2fe452a26450907bd429e3d2257a
didemertens <d.g.j.mertens@gmail.com>
2020-02-14 12:17:09 +0000

    deleted old navs

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -39,10 +39,7 @@ class Navbar extends React.Component {
               <span></span>
             </label>
           </div>
-
           <div className="nav-links">
-
-
             {!FrontAuth.isAuthenticated()
               ?
               <>
@@ -63,61 +60,6 @@ class Navbar extends React.Component {
               null}
           </div>
         </div>
-
-
-
-
-        {/*           
-          <div className="navBar-right">
-            {!FrontAuth.isAuthenticated()
-              ?
-              <>
-                <Link to="/register" className="navbar-item">Register</Link>
-                <Link to="/login" className="navbar-item">Login</Link>
-              </>
-              :
-              null
-            }
-            {FrontAuth.isAuthenticated()
-              ?
-              <>
-                <Link to="/create" className="navbar-item">Create an Event</Link>
-                <Link className="navbar-item" to={`/profile/${FrontAuth.getPayload().sub}`}><FaUserAlt /></Link>
-                <a className="navbar-item" href="#" onClick={this.handleLogout}>Logout</a>
-              </>
-              :
-              null
-            }
-          </div>
-        </div> */}
-
-
-        {/* <div className="row navbar" id="nav">
-          <ul>
-
-            <div className="two columns nav-link">
-              <li><Link to="/" className="navbar-item">Home</Link></li>
-            </div>
-            <div className="two columns nav-link">
-              {FrontAuth.isAuthenticated() && <li><Link to="/create" className="navbar-item">Create an Event</Link></li>}
-            </div>
-            <div className="four columns">
-              <p></p>
-            </div>
-            <div className="two columns nav-link">
-              {!FrontAuth.isAuthenticated() && <li><Link to="/register" className="navbar-item">Register</Link></li>}
-            </div>
-            <div className="two columns nav-link">
-              {!FrontAuth.isAuthenticated() && <li><Link to="/login" className="navbar-item">Login</Link></li>}
-            </div>
-            <div className="two columns nav-link">
-              {FrontAuth.isAuthenticated() && <li><Link className="navbar-item" to={`/profile/${FrontAuth.getPayload().sub}`}><FaUserAlt /></Link></li>}
-            </div>
-            <div className="two columns nav-link">
-              {FrontAuth.isAuthenticated() && <a className="navbar-item" href="#" onClick={this.handleLogout}>Logout</a>}
-            </div>
-          </ul>
-        </div> */}
       </>
     )
   }
